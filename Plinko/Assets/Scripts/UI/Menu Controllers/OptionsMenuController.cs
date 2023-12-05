797c07f3695f8519fc1100b821ce41cc63e31c69
Be3pReDeL <bogatovsergey111@gmail.com>
2023-12-05 22:05:44 +0300

    Added Privacy Policy button and Share Button

--- Plinko/Assets/Scripts/UI/Menu Controllers/OptionsMenuController.cs
+++ Plinko/Assets/Scripts/UI/Menu Controllers/OptionsMenuController.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using System.Collections.Generic;
+using NativeShareNamespace;
 
 public class OptionsMenuController : UIController
 {
     [SerializeField] private float _duration = 1f;
+
+    [SerializeField] private string _shareURL = "https://google.com";
+    [SerializeField] private GameObject _privacyPolicyWebView;
 
     public void Start()
     {
@@ -19,5 +23,16 @@ public class OptionsMenuController : UIController
         {
             _tweenObjects[i].Disappear(_duration);
         }
+    }
+
+    public void ShareButton(){
+        new NativeShare()
+			.SetSubject("Plinker!").SetText("Check this cool Plinker game! " + _shareURL).SetUrl(_shareURL)
+			.SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+			.Share();
+    }
+
+    public void PrivacyPolicy(){
+        Instantiate(_privacyPolicyWebView);
     }
 }
